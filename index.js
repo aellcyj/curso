77482d409bfef78e48266d6f53adc2f6a2273e05
aellcyj <33558255+aellcyj@users.noreply.github.com>
2021-12-20 15:50:02 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+document.querySelector('button').addEventListener('click', function(){
+
+    alert('button');
+})
+
+
+document.getElementById('form-user-create').addEventListener('submit', function () {
+
+    alert('chamou o evento');
+
+});
+
+
+
+
+
+
